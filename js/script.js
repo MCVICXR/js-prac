80e0e38a40de5a582ef69057ca5f28413497f5ea
MCVICXR <robert@endline.io>
2017-05-09 18:07:34 -0700

    made changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 console.log("Fuck you, Rob");
 console.warn("What the fuck are you doing?");
 console.error("god help me");
+
+var foo = "hello",
+bar = "world",
+baz = 3;
+
+var foo = "hello";
+var bar = "world";
+var baz = 3;
